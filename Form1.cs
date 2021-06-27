7755987139c54f5c71ecaf4a48a6c4f66a9c0961
lynx <84665734+setsuna-dayo@users.noreply.github.com>
2021-06-27 11:57:01 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace muteAdio
+{
+    public partial class Form1 : Form
+    {
+        public static uint SND_ASYNC = 0x0001;
+        public static uint SND_FILENAME = 0x00020000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Dispose();
+            this.Dispose();
+            this.Close();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.mute);
+            sp.PlayLooping();
+        }
+    }
+}
